570e65a1161eae33d0256c9cba0fec687e98d8b4
prabalchhatkuli <pchhatku@ramapo.edu>
2019-05-16 19:23:50 -0400

    all documentation completed for .cpp files of classes

--- Classes and headers/Statement.cpp
+++ Classes and headers/Statement.cpp
@@ -11,11 +11,51 @@ Statement::~Statement()
 {
 }
 
+
+/**/
+/*
+Statement::GetStatement()
+
+NAME
+		Statement::GetStatement - get the statement of a specific number in the record
+SYNOPSIS
+		string Statement::GetStatement(int a_statementNum);
+			a_statementNum             --> the number of statement in the vector
+DESCRIPTION
+		gets a statement from m_statements vector.
+RETURNS
+		a string that contains a statement
+AUTHOR
+		Prabal Chhatkuli
+DATE
+		8:45pm 4/18/2019
+*/
+/**/
 string Statement::GetStatement(int a_statementNum)
 {
 	return m_statements[a_statementNum];
-}
+}/*string Statement::GetStatement(int a_statementNum);*/
 
+/**/
+/*
+Statement::GetLabelLocation()
+
+NAME
+		Statement::GetLabelLocation - get the statement of a specific number in the record
+SYNOPSIS
+		int Statement::GetLabelLocation(string a_string);
+			a_string             --> the label whose value needs to be found
+DESCRIPTION
+		if a_string is found in the m_labelToStatement map, the integer is returned , if not found an error message is given and the
+		program is terminated.
+RETURNS
+		integer that gives the location of the label in the map of labels
+AUTHOR
+		Prabal Chhatkuli
+DATE
+		8:45pm 4/18/2019
+*/
+/**/
 int Statement::GetLabelLocation(string a_string)
 {
 	if (m_labelToStatement.find(a_string) != m_labelToStatement.end())
@@ -27,8 +67,28 @@ int Statement::GetLabelLocation(string a_string)
 		cerr << "BugBug : the label was not found in the map of the labels" << endl;
 		exit(0);
 	}
-}
+}/*int Statement::GetLabelLocation(string a_string);*/
 
+
+/**/
+/*
+Statement::colonPresence()
+
+NAME
+		Statement::colonPresence - check if a colon exits in a string
+SYNOPSIS
+		bool Statement::colonPresence(string findcolon);
+			findcolon             --> a string/statement
+DESCRIPTION
+		iterates through a string and checks each character with colon
+RETURNS
+		a boolean value: true if colon exits , false if does not
+AUTHOR
+		Prabal Chhatkuli
+DATE
+		5:35pm 5/7/2019
+*/
+/**/
 bool Statement::colonPresence(string findcolon)
 {
 	for (int i = 0; i < findcolon.length(); i++)
@@ -41,6 +101,26 @@ bool Statement::colonPresence(string findcolon)
 	return false;
 }/*bool Statement::colonPresence(string findcolon)*/
 
+/**/
+/*
+Statement::quotesBeforeColon()
+
+NAME
+		Statement::quotesBeforeColon - check if a colon exits in a string before encountering a quotes
+SYNOPSIS
+		bool Statement::quotesBeforeColon(string findcolon);
+			findcolon             --> a string/statement
+DESCRIPTION
+		iterates through a string and checks each character for quotes, if a quotes shows up a flag called colonBeforeQuotes is set
+		if a colon is found before this happens then the function returns true. Then the functions iterates until the function ends.
+RETURNS
+		a boolean value: true if colon exists before quotes , false if quotes exists before colon
+AUTHOR
+		Prabal Chhatkuli
+DATE
+		8:45pm 4/18/2019
+*/
+/**/
 bool Statement::quotesBeforeColon(string findcolon)
 {
 	bool colonBeforeQuotes = false;
@@ -56,8 +136,29 @@ bool Statement::quotesBeforeColon(string findcolon)
 		}
 	}
 	return false;
-}/*bool Statement::quotesBeforeColon(string findcolon)*/
+}/*bool Statement::quotesBeforeColon(string findcolon);*/
 
+
+/**/
+/*
+Statement::RecordStatements()
+
+NAME
+		Statement::RecordStatements - check if a colon exits in a string before encountering a quotes
+SYNOPSIS
+		void Statement::RecordStatements(string a_sourceFileName);
+			a_sourceFilename             --> a string that contains the name of the filename to record statements from
+DESCRIPTION
+		Opens and reads the file one at a time, removes any empty lines, then iterates through each statement and uses
+		functions colonPresence and quotesBeforeColon to find labels and record them in a map
+RETURNS
+		void
+AUTHOR
+		Prabal Chhatkuli
+DATE
+		3:40pm 4/15/2019
+*/
+/**/
 void Statement::RecordStatements(string a_sourceFileName)//passing to the function the source file
 {
 	ifstream inf(a_sourceFileName);
@@ -116,4 +217,4 @@ void Statement::RecordStatements(string a_sourceFileName)//passing to the functi
 		cout << (*it).first << "\t" <<(*it).second<<endl;
 	}
 	
-}/*void Statement::RecordStatements(string a_sourceFileName)*/
+}/*void Statement::RecordStatements(string a_sourceFileName);*/
--- Classes and headers/SymbolTable.cpp
+++ Classes and headers/SymbolTable.cpp
@@ -11,16 +11,76 @@ SymbolTable::~SymbolTable()
 {
 }
 
+/**/
+/*
+SymbolTable::RecordVariableValue()
+
+NAME
+		SymbolTable::RecordVariableValue - record the value of a variable
+SYNOPSIS
+		void SymbolTable::RecordVariableValue(string a_variable, double a_value);
+			a_variable             --> the string/key to access the map
+			a_value             --> the value to be stored
+DESCRIPTION
+		uses a_variable as a key to store the value in m_SymnolTable
+RETURNS
+		void
+AUTHOR
+		Prabal Chhatkuli
+DATE
+		2:45am 4/19/2019
+*/
+/**/
 void SymbolTable::RecordVariableValue(string a_variable, double a_value)
 {
 	m_SymbolTable[a_variable] = a_value;
-}
+}/*void SymbolTable::RecordVariableValue(string a_variable, double a_value);*/
 
+/**/
+/*
+SymbolTable::replaceVariableValue()
+
+NAME
+		SymbolTable::replaceVariableValue - record the value of a variable
+SYNOPSIS
+		void SymbolTable::RecordVariableValue(string a_variable, double a_value);
+			a_variable             --> the string/key to access the map
+			a_value             --> the value to be stored
+DESCRIPTION
+		replaces the value of a map/key.
+RETURNS
+		void
+AUTHOR
+		Prabal Chhatkuli
+DATE
+		5:00pm 4/20/2019
+*/
+/**/
 void SymbolTable::replaceVariableValue(string a_variable, double a_value)
 {
 	m_SymbolTable[a_variable] = a_value;
 }
 
+/**/
+/*
+SymbolTable::GetVariableValue()
+
+NAME
+		SymbolTable::GetVariableValue - Get the value of a variable.  Returns false if the variable does not exist.
+SYNOPSIS
+		bool SymbolTable::GetVariableValue(string a_variable, double &a_value);
+			a_variable             --> the variable whose value is needed
+			a_value             --> the container variable passed by reference which will contain the value of the variable
+DESCRIPTION
+		searches the m_SymbolTable map for a variabel if found passes it by reference, if not, returns a boolean (false)
+RETURNS
+		true if a value exists, false if it does not; passed by reference is the value of variable in a_value if it exists
+AUTHOR
+		Prabal Chhatkuli
+DATE
+		11:30pm 5/29/2019
+*/
+/**/
 bool SymbolTable::GetVariableValue(string a_variable, double &a_value)
 {
 	if (m_SymbolTable.find(a_variable) == m_SymbolTable.end())
@@ -32,4 +92,4 @@ bool SymbolTable::GetVariableValue(string a_variable, double &a_value)
 		a_value = m_SymbolTable.find(a_variable)->second;
 		return true;
 	}
-}
+}/*bool SymbolTable::GetVariableValue(string a_variable, double &a_value);*/
